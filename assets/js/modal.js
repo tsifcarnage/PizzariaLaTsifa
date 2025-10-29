e83168b1ed0040c6bb4d18d48024a14c630af0c6
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-10-29 13:35:12 +0100

    updatee

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -16,12 +16,29 @@ document.addEventListener("DOMContentLoaded", (e) => {
       });
     });
   });
-  function contact() {
-    const formCnt = document.querySelector(".formCnt");
-    const rep = document.querySelector(".rep");
-    let nom = document.getElementById("nom");
-    let mail = document.getElementById("mail");
-    let num = document.getElementById("num");
-    let sujet = document.getElementById("sujet");
-  }
 });
+function cF(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function contact(e) {
+  e.preventDefault();
+  const formCnt = document.querySelector(".formCnt");
+  const rep = document.querySelector(".rep");
+
+  let nom = document.getElementById("nom").value;
+  let mail = document.getElementById("mail").value;
+  let num = document.getElementById("num").value;
+  let sujet = document.getElementById("sujet").value;
+
+  let txt = `Bonjour ${cF(nom)}, votre sujet "${cF(
+    sujet
+  )}" est bien traité. Vous avez bien comme numero de telephone ${cF(
+    num
+  )}, et email ${mail.toLowerCase()}.Je vous renvoie une réponse très vite.`;
+
+  formCnt.style.display = "none";
+  rep.style.display = "block";
+  document.getElementById("response").textContent = txt;
+}
